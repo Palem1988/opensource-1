d8a7614ddd331d43ef08d30689ad5415ae827ff1
opensource@momomo.com <joseph.seifeddine@sbab.se>
2019-07-21 22:25:45 +0200

    Update

--- momomo.com.shell.style.sh
+++ momomo.com.shell.style.sh
@@ -170,8 +170,8 @@ momomo.com.shell.style() {
 	
 	#############################################
 	     #### We choose implementation ####
-	# momomo.com.shell.style.interface.implemented.using.echo
-	momomo.com.shell.style.interface.implemented.using.printf
+	momomo.com.shell.style.interface.implemented.using.echo
+	# momomo.com.shell.style.interface.implemented.using.printf
 	#############################################
 	
 	local  user="$(momomo.com.shell.style.color "${PRIVATE_MOMOMO_COM_COLOR_CYAN}"   "\u" )"
